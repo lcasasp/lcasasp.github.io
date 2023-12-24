175384de13b5cf2e06dd79bcda8fd7cbd1b0853d
Lucas Casas <lucascasasp@icloud.com>
2023-12-23 21:16:12 -0700

    Fixed mobile pdf inlay

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,17 +1,32 @@
 <script>
-	import Chip from '$lib/components/Chip/Chip.svelte';
-	import CommonPage from '$lib/components/CommonPage.svelte';
-	import { RESUME } from '$lib/params';
+    import { onMount } from 'svelte';
+    import Chip from '$lib/components/Chip/Chip.svelte';
+    import CommonPage from '$lib/components/CommonPage.svelte';
+    import { RESUME } from '$lib/params';
 
-	const { item, title } = RESUME;
+    const { item, title } = RESUME;
+    let isMobile = false;
+
+    onMount(() => {
+        isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+    });
 </script>
 
 <CommonPage {title}>
     <div class="resume">
-        {#if item}
-			<div class="pdf-viewer">
-                <embed src={item} type="application/pdf" width="100%" height="600px" />
-            </div>
+		{#if item}
+			{#if isMobile}
+					<a href={item} download="{title}.pdf">
+						<Chip size={'1.25em'}>Download Resume</Chip>
+					</a>
+            {:else}
+                <div class="pdf-viewer">
+					<embed src={item} type="application/pdf" width="100%" height="600px" /> 
+				</div>
+                <a href={item} download="{title}.pdf">
+                    <Chip size={'1.25em'}>Download</Chip>
+                </a>
+            {/if}
         {:else}
             <Chip>Ooops! no CV at the moment.</Chip>
         {/if}
@@ -31,6 +46,7 @@
         }
 
         .pdf-viewer {
+			background-color: white;
             width: 100%; // Adjust width as needed
             max-width: 800px; // Adjust maximum width as needed
             border: 1px solid #ddd; // Optional border for the PDF viewer
